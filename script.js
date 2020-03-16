1c4a8bbfeae0905864077c8613b1264c67c19e9e
Nastya <an.tabachinskaya@gmail.com>
2020-03-16 22:42:19 +0300

    feat: add active elements of menu and links for items of menu

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+var nav = document.getElementById("hor-menu-id");
+var items = nav.getElementsByClassName("hor-menu-item");
+for (var i = 0; i < items.length; i++) {
+    items[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
